0119ca1d3f214e1563eb2446554380cfdc978722
Mouad Zouadi <mouad.zouadi@etu.univ-orleans.fr>
2026-01-12 12:58:47 +0100

    etape 5 : Visualisation faite

--- src/main.py
+++ src/main.py
@@ -7,4 +7,5 @@ URL_AVIS = "https://cert.ssi.gouv.fr/avis/feed/"
 if __name__ == "__main__":
     manager = DataManager()
     manager.Step(URL_ALERTE)
+    #manager.Step(URL_AVIS)
     #manager.EnrichAllCVE()
